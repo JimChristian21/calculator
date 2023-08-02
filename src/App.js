6d8f6add0ca0ff9d75bcfd4c801647165fac5d4b
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2023-08-02 13:56:28 +0800

    updated classes and display

--- src/App.js
+++ src/App.js
@@ -4,40 +4,38 @@ function App() {
   return (
     <div className="bg-slate-200 min-h-screen flex flex-row justify-center items-center">
       <div className="container w-full sm:w-1/2 md:w-1/3 bg-slate-400 rounded-xl">
-        <div id="screen" className="bg-white m-5 rounded-md h-20">
-          <div className="my-2">
-            sddafsd
-          </div>
+        <div id="screen" className="bg-white m-5 rounded-md h-20 flex items-end justify-end">
+          <p className="text-3xl">sample</p>
         </div>
         <div id="input" className="m-5">
           <div>
-            <button className="w-1/4 h-16 bg-slate-500">AC</button>
-            <button className="w-1/4 h-16 bg-slate-500">+/-</button>
-            <button className="w-1/4 h-16 bg-slate-500">%</button>
-            <button className="w-1/4 h-16 bg-slate-500">/</button>
+            <button className="w-1/4 h-16 bg-slate-500 border-2 border-slate-700 hover:bg-slate-600">AC</button>
+            <button className="w-1/4 h-16 bg-slate-500 border-2 border-slate-700 hover:bg-slate-600">+/-</button>
+            <button className="w-1/4 h-16 bg-slate-500 border-2 border-slate-700 hover:bg-slate-600">%</button>
+            <button className="w-1/4 h-16 bg-slate-500 border-2 border-slate-700 hover:bg-slate-600">/</button>
           </div>
           <div>
-            <button className="w-1/4 h-16 bg-slate-500">7</button>
-            <button className="w-1/4 h-16 bg-slate-500">8</button>
-            <button className="w-1/4 h-16 bg-slate-500">9</button>
-            <button className="w-1/4 h-16 bg-slate-500">X</button>
+            <button className="w-1/4 h-16 bg-slate-500 border-2 border-slate-700 hover:bg-slate-600">7</button>
+            <button className="w-1/4 h-16 bg-slate-500 border-2 border-slate-700 hover:bg-slate-600">8</button>
+            <button className="w-1/4 h-16 bg-slate-500 border-2 border-slate-700 hover:bg-slate-600">9</button>
+            <button className="w-1/4 h-16 bg-slate-500 border-2 border-slate-700 hover:bg-slate-600">X</button>
           </div>
           <div>
-            <button className="w-1/4 h-16 bg-slate-500">4</button>
-            <button className="w-1/4 h-16 bg-slate-500">5</button>
-            <button className="w-1/4 h-16 bg-slate-500">6</button>
-            <button className="w-1/4 h-16 bg-slate-500">-</button>
+            <button className="w-1/4 h-16 bg-slate-500 border-2 border-slate-700 hover:bg-slate-600">4</button>
+            <button className="w-1/4 h-16 bg-slate-500 border-2 border-slate-700 hover:bg-slate-600">5</button>
+            <button className="w-1/4 h-16 bg-slate-500 border-2 border-slate-700 hover:bg-slate-600">6</button>
+            <button className="w-1/4 h-16 bg-slate-500 border-2 border-slate-700 hover:bg-slate-600">-</button>
           </div>
           <div>
-            <button className="w-1/4 h-16 bg-slate-500">1</button>
-            <button className="w-1/4 h-16 bg-slate-500">2</button>
-            <button className="w-1/4 h-16 bg-slate-500">3</button>
-            <button className="w-1/4 h-16 bg-slate-500">+</button>
+            <button className="w-1/4 h-16 bg-slate-500 border-2 border-slate-700 hover:bg-slate-600">1</button>
+            <button className="w-1/4 h-16 bg-slate-500 border-2 border-slate-700 hover:bg-slate-600">2</button>
+            <button className="w-1/4 h-16 bg-slate-500 border-2 border-slate-700 hover:bg-slate-600">3</button>
+            <button className="w-1/4 h-16 bg-slate-500 border-2 border-slate-700 hover:bg-slate-600">+</button>
           </div>
           <div>
-            <button className="w-2/4 h-16 bg-slate-500">0</button>
-            <button className="w-1/4 h-16 bg-slate-500">.</button>
-            <button className="w-1/4 h-16 bg-slate-500">=</button>
+            <button className="w-2/4 h-16 bg-slate-500 border-2 border-slate-700 hover:bg-slate-600">0</button>
+            <button className="w-1/4 h-16 bg-slate-500 border-2 border-slate-700 hover:bg-slate-600">.</button>
+            <button className="w-1/4 h-16 bg-slate-500 border-2 border-slate-700 hover:bg-slate-600">=</button>
           </div>
         </div>
       </div>
